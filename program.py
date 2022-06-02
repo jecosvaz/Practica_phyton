bfb436d94b78da8cd405e0a5b86f36669d217610
jecosvaz <jecosvaz@gmail.com>
2022-06-01 21:33:37 -0500

    kata1

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-# program.py    
+# program.py
 
 from numpy import product
 from datetime import date
@@ -6,14 +6,14 @@ from datetime import date
 sum = 1+2
 print(sum)
 # producto del resultado de la suma por dos
-product=sum*2
+product = sum*2
 print('')
 # Impresión en pantalla print('Hola desde la consola')
 print('hola desde la consola')
 # Pintamos el resultado del producto en consola
 print(product)
 # funcion type
-distancia_a_alfa_centauri=4.367
+distancia_a_alfa_centauri = 4.367
 type(distancia_a_alfa_centauri)
 # Mandamos a llamar el dia de hoy con la funcion today de date
 # Para que la funcion "date.today()" pueda pintarse en la consola
@@ -22,7 +22,13 @@ type(distancia_a_alfa_centauri)
 print("Today´s date is "+str(date.today()))
 print('')
 print('bienvenido al programa de bienvenida')
-name=input('Introduzca su nombre: ')
+name = input('Introduzca su nombre: ')
 print('Saludos desde la consola ' + name)
-
-
+print('')
+# Funcion calculadora
+# para el caso de que no salga un resultado concatenado print(first_numbre+second_number) se requiere declarar int() para que los
+# valores introducidos en la operacion sean datos enteros print(int(first_numbre)+int(second_number)) 
+print('Calculadora')
+first_numbre = input('Primer numero: ')
+second_number = input('Segundo numero: ')
+print(int(first_numbre)+int(second_number))
